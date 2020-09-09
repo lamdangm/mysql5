2e2cccb5a0d25d81ef42c80cd0b8cc435ea97299
lamdang0 <43851947+lamdang0@users.noreply.github.com>
2020-09-09 16:36:54 +0700

    Update live_replicate.sh

--- live_replicate.sh
+++ live_replicate.sh
@@ -1,73 +1,94 @@
 #!/bin/bash
 set -euo pipefail
 
-DUMP_FILE="/tmp/export-$(date +"%d%m%Y").sql"
-
-#MASTER
+#MASTER info
 MIP="192.168.14.54"
 MPORT="3307"
 MUSER="root"
 MPWD="admin"
 MDATA="/data/mysql/MYSQL51"
-MRUN="/data/mysql/MYSQL51/data/mysql/mysql.server"
+MRUN="/etc/init.d/mysql51"
 MBIN="/usr/local/mysql51/bin/mysql"
 MMYSQLDUMP="/usr/local/mysql51/bin/mysqldump"
 RUSER="repl"
 RPWD="slavepassword"
 #MSERVER-ID="1"
 
-#SLAVE
-SIP="192.168.14.55"
+#SLAVE info
+SIP="192.168.14.61"
 SPORT="3307"
 SUSER="root"
 SPWD="admin"
 SDATA="/data/mysql/MYSQL51"
-SRUN="/data/mysql/MYSQL51/data/mysql/mysql.server"
 SBIN="/usr/local/mysql51/bin/mysql"
+SRUN="/etc/init.d/mysql51"
 SLAVE_OK="OK"
 #SSERVER-ID="2"
 
-#############
 
-#create user replicate to
+echo "Create user *$RUSER* to replicate to"
 $MBIN "-u$MUSER" "-p$MPWD" <<-EOSQL &
 CREATE USER '$RUSER'@'%' IDENTIFIED BY '$RPWD';
 GRANT REPLICATION SLAVE ON *.* TO '$RUSER'@'%';
 FLUSH PRIVILEGES;
-FLUSH TABLES WITH READ LOCK;
 EOSQL
 
-#echo "Waiting for database to be locked"
-sleep 3
+####################
+#############
+echo "Starting setup Replication for MASTER:$MIP and SLAVE:$SIP."
+echo "1. Stop Master"
+$MRUN stop
+echo "MYSQL on MASTER machine has been stopped"
 
-# Dump the database (to the client executing this script) while it is locked
-echo "  - Dumping database to $DUMP_FILE"
-$MMYSQLDUMP "-u$MUSER" "-p$MPWD" --all-database --master-data > $DUMP_FILE
-echo "  - Dump completed."
+echo "2. Stop Slave"
+ssh root@$SIP <<END_SCRIPT
+$SBIN "-u$SUSER" "-p$SPWD" -e "STOP SLAVE\G;"
+$SRUN stop
+echo "MYSQL on slave machine has been stopped"
+exit
+END_SCRIPT
 
-echo "Get LOG_FILE and LOG_POS of Master"
+echo "3. Start to truncate Slave and Master"
+
+echo "Truncate log file on Master"
+rm -rf $MDATA/log/*
+echo "Truncate log files on Master completed"
+
+echo "Truncate databases and logs on Slave"
+ssh root@$SIP <<END_SCRIPT
+rm $SDATA/log/* -rf
+rm $SDATA/data/* -rf
+echo "Delete logs and databases on slave completed"
+$SBIN "-u$SUSER" "-p$SPWD" -e "STOP SLAVE\G;"
+$SRUN stop
+echo "MYSQL on slave machine has been stopped"
+exit
+END_SCRIPT
+
+
+# Sync database
+echo "Syncing databases to slave" #Deletes files in the destination directory if they don't exist in the source directory
+rsync -avz --delete $MDATA/data/ root@$SIP:$SDATA/data
+echo "Sync databases completed"
+
+echo "5. Starting MYSQL on master"
+$MRUN start
+sleep 2
+
+
+#Get master info
+echo "Get LOG_FILE and LOG_POS of MASTER"
 MASTER_STATUS=$($SBIN -uroot --password="$MPWD" --port=$MPORT -ANe "SHOW MASTER STATUS\G;" | awk '{print $1 " " $2}')
 LOG_FILE=$(echo $MASTER_STATUS | cut -f3 -d ' ')
 LOG_POS=$(echo $MASTER_STATUS | cut -f4 -d ' ')
 echo "Current log file is $LOG_FILE and log position is $LOG_POS"
 
-#When finished, unlock table
-$MBIN "-u$MUSER" "-p$MPWD" <<-EOSQL &
-UNLOCK TABLES;
-EOSQL
-echo "Master database unlocked"
 
-echo "Slave: $SIP"
-echo "Copy file dump from Master to Slave"
-rsync -avz --delete $DUMP_FILE root@$SIP:$DUMP_FILE
-
-echo "---Config Slave---"
+echo "6. Start MYSQL on Slave "
 ssh root@$SIP <<END_SCRIPT1
-
-echo "Import and start slave"
-$SBIN "-u$SUSER" "-p$SPWD" < $DUMP_FILE
-
-echo "Setting up slave replication on Slave"
+echo "Starting MYSQL on slave"
+$SRUN start
+echo "Setting up slave replication on Slave to accept $MIP as Master"
 $SBIN "-u$SUSER" "-p$SPWD" <<-EOSQL1 &
 STOP SLAVE;
 CHANGE MASTER TO MASTER_HOST='$MIP',
@@ -76,32 +97,35 @@ MASTER_PASSWORD='$RPWD',
 MASTER_PORT=$MPORT,
 MASTER_LOG_FILE='$LOG_FILE',
 MASTER_LOG_POS=$LOG_POS;
+FLUSH PRIVILEGES;
 START SLAVE;
-
 EOSQL1
 
+echo "Setup slave config completed"
 #Wait for slave to get started and have the correct status
 sleep 2
 
-#Check if replication status is OK
+echo "Checking SLAVE status"
 SLAVE_OK=$($SBIN "-u$SUSER" "-p$SPWD" -e "SHOW SLAVE STATUS\G;" | grep "Slave_IO_Running: Yes")
 if [ -z "$SLAVE_OK" ]; then
         echo "Slave_IO is not running"
 else
         echo "Slave_IO is running"
-fi      
+fi
 exit
 END_SCRIPT1
 
-
-
 ##master info
-tmp_db="tmp$(date +"%Y%m%d%H%M")"
-$MBIN "-u$MUSER" "-p$MPWD" -e "CREATE DATABASE $tmp_db;"
-$MBIN "-u$MUSER" "-p$MPWD" -e "SHOW DATABASES;"
+echo "Checking MASTER status"
 $MBIN "-u$MUSER" "-p$MPWD" -e "SHOW MASTER STATUS\G;"
 
 ##test on slave
-$SBIN "-u$SUSER" "-p$SPWD" --port=$SPORT -h $SIP -e "SHOW DATABASES\G;"
-$SBIN "-u$SUSER" "-p$SPWD" --port=$SPORT -h $SIP -e "SHOW SLAVE STATUS\G;"
-$SBIN "-u$SUSER" "-p$SPWD" --port=$SPORT -h $SIP -e "SHOW PROCESSLIST\G;"
+echo "Checking SLAVE status"
+ssh root@$SIP <<END_SCRIPT2
+echo "Current databases list on Slave:"
+$SBIN "-u$SUSER" "-p$SPWD" --port=$SPORT -e "SHOW DATABASES\G;"
+echo "Slave status:"
+$SBIN "-u$SUSER" "-p$SPWD" --port=$SPORT -e "SHOW SLAVE STATUS\G;"
+echo "Process list"
+$SBIN "-u$SUSER" "-p$SPWD" --port=$SPORT -e "SHOW PROCESSLIST\G;"
+END_SCRIPT2
